10d52e6b4617fb4cd20c120dc3e1b817fcb8e1be
InsiyaRampurawala <insiyahbeguwala53@gmail.com>
2026-03-05 23:35:40 +0530

    Admin django

--- hardware_inventory_system/urls.py
+++ hardware_inventory_system/urls.py
@@ -5,3 +5,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', include('shop.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
 def home(request):
     products = Product.objects.all()
     return render(request, "product_list.html", {"products": products})
+
 
 def product_detail(request, id):
 
