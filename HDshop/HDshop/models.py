d98c894933e2995a8ffcfdb11ab40ac4e36e065c
InsiyaRampurawala <insiyahbeguwala53@gmail.com>
2026-03-09 15:41:56 +0530

    admin side formating

--- HDshop/HDshop/models.py
+++ HDshop/HDshop/models.py
@@ -44,3 +44,20 @@ class RegisterUser(models.Model):
 
     def __str__(self):
         return self.first_name
+    from django.db import models
+
+class Invoice(models.Model):
+    invoice_no = models.CharField(max_length=50)
+    date = models.DateField()
+    customer = models.CharField(max_length=100)
+    items = models.IntegerField()
+    total = models.FloatField()
+    from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.name
